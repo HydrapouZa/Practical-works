84c5d3f02849bc282b39ebd50de95919f80033d3
Кирилл Ермолаев <ermolaevkirill70@gmail.com>
2025-11-01 17:15:41 +0300

    Add files via upload

---
+++ Practical12/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac12
+{
+    class Settings
+    {
+        private static readonly string configPath;
+        static Settings()
+        {
+            configPath = "C:/Users/Me/source/repos/Prac12/App.config";
+        }
+        public static void COutPath()
+        {
+            Console.WriteLine(configPath);
+        }
+    }
+}
